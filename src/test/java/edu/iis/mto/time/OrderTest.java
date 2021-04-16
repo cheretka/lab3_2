89265c94df0d79143b64c556ba3c22f821f84c26
CheretKa <ocheretianfws@gmail.com>
2021-04-16 20:49:37 +0300

    add tests with valid hours

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -2,18 +2,87 @@ package edu.iis.mto.time;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import java.lang.reflect.InvocationHandler;
 
 
+
+@ExtendWith(MockitoExtension.class)
 class OrderTest {
 
+    private static final long VALID_PERIOD_HOURS = 20;
+    private static final long UNVALID_PERIOD_HOURS = 40;
+
+    @Mock
+    private Clock variableClock;
+
+    private Order order;
+    private Instant startTimeCount;
+    private Instant endTimeCount;
+
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp() throws Exception {
+        order = new Order(variableClock);
+        startTimeCount = Instant.now();
+
+        when(variableClock.getZone())
+                .thenReturn(ZoneId.systemDefault());
+    }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void when_ElapsedZeroSeconds_Expect_ConfirmedOrderState() {
+        endTimeCount = startTimeCount;
+
+        when(variableClock.instant())
+                .thenReturn(startTimeCount)
+                .thenReturn(endTimeCount);
+
+        order.submit();
+
+        assertDoesNotThrow(() -> order.confirm());
+        assertEquals(order.getOrderState(), Order.State.CONFIRMED);
+    }
+
+    @Test
+    void when_ElapsedValidHours_Expect_ConfirmedOrderState() {
+        endTimeCount = startTimeCount.plus(VALID_PERIOD_HOURS, ChronoUnit.HOURS);
+
+        when(variableClock.instant())
+                .thenReturn(startTimeCount)
+                .thenReturn(endTimeCount);
+
+        order.submit();
+
+        assertDoesNotThrow(() -> order.confirm());
+        assertEquals(order.getOrderState(), Order.State.CONFIRMED);
+    }
+
+    @Test
+    void when_ElapsedOneSecondBeforeInvalidHours_Expect_ConfirmedOrderState() {
+        endTimeCount = startTimeCount.plus(24, ChronoUnit.HOURS).plus(59, ChronoUnit.MINUTES).plus(59, ChronoUnit.SECONDS);
+
+        when(variableClock.instant())
+                .thenReturn(startTimeCount)
+                .thenReturn(endTimeCount);
+
+        order.submit();
+
+        assertDoesNotThrow(() -> order.confirm());
+        assertEquals(order.getOrderState(), Order.State.CONFIRMED);
     }
 
 }
